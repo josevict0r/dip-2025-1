fdca05d5965fe308881ecc8498b42ccd80934649
José Victor <josevictor.vd25@gmail.com>
2025-07-30 21:46:02 -0300

    task 2 successfully done

--- tasks/task-02-salt-and-pepper/task-02-salt-and-pepper.py
+++ tasks/task-02-salt-and-pepper/task-02-salt-and-pepper.py
@@ -6,9 +6,13 @@ def create_salt_and_pepper_noise(height=100, width=100, salt_prob=0.05, pepper_p
     pepper (I = -1.0) noise with respective probability distributions
     equal to salt_prob and pepper_prob. Pixels without noise have values of 0.5.
     """
-    ### START CODE HERE ###
-    # TODO
-    ### END CODE HERE ###
+    img = np.full((height, width), 0.5)
+    mascara_aleatoria = np.random.rand(height, width)
+
+    img[mascara_aleatoria < salt_prob] = 1.0
+    img[(mascara_aleatoria >= salt_prob) & (mascara_aleatoria < (salt_prob+pepper_prob))] = -1.0
+    
+
     return img
 
 def main():
